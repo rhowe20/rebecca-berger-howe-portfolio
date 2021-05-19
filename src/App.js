5a33beb19dcb4358a177b0d51f88ec68ee2a5705
Rebecca Howe <Rebeccahowe20@gmail.com>
2021-05-18 19:06:10 -0700

    add styling and functional navbar

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React from "react";
 import { HashRouter, Switch, Route } from "react-router-dom";
+import "./App.css";
 import Homepage from "../src/pages/home";
 import Projects from "../src/pages/projects";
 import Contact from "../src/pages/contact";
 import About from "../src/pages/about"
 import NavBar from "../src/components/nav/nav";
 import Footer from "../src/components/footer/footer";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const App = () => {
   return (
     <HashRouter>
       <NavBar/>
-        <Switch>
-          <Route exact path='/' component={Homepage}></Route>
+        <Switch >
+          <Route exact path='/*' component={Homepage}></Route>
           <Route path='/about' component={About}></Route>
           <Route path='/project' component={Projects}></Route>
           <Route path='/contact' component={Contact}></Route>
--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -1,8 +1,25 @@
 import React from "react"
+import {Navbar, Nav, NavDropdown } from "react-bootstrap"
+import "./nav.css";
 
 function NavBar(){
     return(
-        <div>navigation</div>
+        <Navbar collapseOnSelect expand="lg" className="color" variant="dark">
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+                <Nav className="mr-auto">
+                <Nav.Link href="#about">Home</Nav.Link>
+                <Nav.Link href="#about">About Me</Nav.Link>
+                <NavDropdown title="Projects" id="collasible-nav-dropdown">
+                    <NavDropdown.Item href="#action/3.1">eList</NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.2">
+                    Dungeons & Dragons Helper
+                    </NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.3">U-Notes</NavDropdown.Item>
+                </NavDropdown>
+                </Nav>
+            </Navbar.Collapse>
+            </Navbar>
     )
 }
 
