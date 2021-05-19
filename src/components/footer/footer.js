3c337f16d7a9f85fccc628bbf141dd315f58363a
Rebecca Howe <Rebeccahowe20@gmail.com>
2021-05-18 19:44:38 -0700

    socials on footer

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,9 +1,16 @@
 import React from "react";
-import "./footer.css"
+import "./footer.css";
+import { SocialIcon } from 'react-social-icons';
+import {Navbar} from "react-bootstrap";
 
 function Footer(){
     return(
-        <div className='footer'>footer</div>
+            <Navbar className="footer" dark>
+                <SocialIcon url="https://www.linkedin.com/in/rebecca-berger-howe/" bgColor="#4BC3B5"/>
+                <SocialIcon url="https://twitter.com/howe_becca" bgColor="#4BC3B5"/>
+                <SocialIcon url="https://www.facebook.com/Rhowe16" bgColor="#4BC3B5"/>
+                <SocialIcon url="https://github.com/rhowe20" bgColor="#4BC3B5"/>
+            </Navbar>
     )
 }
 
